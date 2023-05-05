c6fef5344a133c78cf9adc2495be78e2eee11343
Lucca Rauédys <luccarauedys@outlook.com>
2023-05-05 00:37:53 -0300

    estrutura inicial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Container, Typography } from "@mui/material";
+
+export default function App() {
+  return (
+    <Container>
+      <Typography variant="h1">XE Currency</Typography>
+    </Container>
+  );
+}
